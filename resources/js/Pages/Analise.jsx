ef7b4a3bf7efb42588e058513d7daeeedf1db297
Leonardo Furmann <leohitnerf@gmail.com>
2024-08-30 23:45:33 -0300

    Legenda

--- resources/js/Pages/Analise.jsx
+++ resources/js/Pages/Analise.jsx
@@ -47,10 +47,7 @@ const Analise = ({ auth }) => {
                     <div>
                         <h1 className='text-gray-100 p-6 font-semibold text-2xl'>Sinais de Pneumonia Encontrados</h1>
                     </div>
-                    <div className={`h-full relative overflow-hidden ${zoom ? 'cursor-zoom-in' : ''}`} 
-                        onMouseMove={handleMouseMove}
-                        onMouseLeave={handleMouseLeave}
-                    >
+                    <div className={`h-full relative overflow-hidden ${zoom ? 'cursor-zoom-in' : ''}`} >
                         <img
                             src={scr}
                             id='img_raiox'
@@ -62,6 +59,8 @@ const Analise = ({ auth }) => {
                              brightness(${brilho}%) 
                              invert(${invert}%)`
                             }}
+                            onMouseMove={handleMouseMove}
+                            onMouseLeave={handleMouseLeave}
                         />
                     </div>
                 </div>
@@ -69,7 +68,7 @@ const Analise = ({ auth }) => {
                     <div className='p-3 me-4'>
                         <h1 className='text-gray-100 font-medium text-xl p-2'>Filtros</h1>
                         <div className='h-1 bg-gray-500 w-full'></div>
-                        <div className='p-6 flex justify-center'>
+                        <div className='p-5 flex justify-center'>
                             <button className={` rounded border-gray-400 border text-white p-2 ${zoom ? 'bg-primary' : 'bg-zinc-900'} `} onClick={() => setZoom(!zoom)}>Zoom</button>
                         </div>
                         <div className='m-3'>
@@ -77,20 +76,33 @@ const Analise = ({ auth }) => {
                             <Range name={'Brilho'} value={brilho} setValue={setBrilho} max={200} />
                             <Range name={'Inversão de cores'} value={invert} setValue={setInvert} max={100} />
                         </div> 
-                        <h1 className='text-gray-100 font-medium text-xl p-2 mt-5'>Alterar Imagem</h1>
+                        <h1 className='text-gray-100 font-medium text-xl p-2 mt-3'>Alterar Imagem</h1>
                         <div className='h-1 bg-gray-500 w-full'></div>    
                         <div className='p-6 flex justify-center content-center xl:flex-col gap-2 flex-wrap'>
                             <button id='original' className='bg-zinc-900 py-2  w-40 text-gray-200 border-gray-400 border rounded-sm text-center hover:bg-zinc-800' onClick={() => setScr(raiox)}>Original</button>
                             <button id='mapa_calor' className='bg-zinc-900 py-2  w-40 text-gray-200 border-gray-400 border rounded-sm text-center hover:bg-zinc-800' onClick={() => setScr(mapaCalor)}>Mapa de Calor</button>
                             <button id='analise' className='bg-zinc-900 py-2  w-40 text-gray-200 border-gray-400 border rounded-sm text-center hover:bg-zinc-800' onClick={() => setScr(imgAnalise)}>Análise</button>                       
                         </div>         
-                        <h1 className='text-gray-100 font-medium text-xl p-2 mt-2'>Legendas</h1>
+                        <h1 className='text-gray-100 font-medium text-xl p-2'>Legendas</h1>
                         <div className='h-1 bg-gray-500 w-full'></div>
-                        <div className='flex m-5 flex-row'>
-                            <div className='flex content-center flex-wrap'>
+                        <div className='flex m-3 flex-col justify-center content-center gap-2'>
+                            <div className='flex content-center gap-2'>
                                 <div className='w-8 h-8 bg-zinc-900 self-center border-gray-400 border rounded-sm '></div>
-                                <p className='text-gray-100'>Sinais de Pneumonia</p>
+                                <p className='text-gray-100 self-center'>Sinais de Pneumonia</p>
                             </div>
+                            <div className='flex content-center gap-2'>
+                                <div className='w-8 h-8 bg-blue-500 self-center border-gray-400 border rounded-sm '></div>
+                                <p className='text-gray-100 self-center'>Baixa Densidade</p>
+                            </div>
+                            <div className='flex content-center gap-2'>
+                                <div className='w-8 h-8 bg-green-400 self-center border-gray-400 border rounded-sm '></div>
+                                <p className='text-gray-100 self-center'>Média Densidade</p>
+                            </div>
+                            <div className='flex content-center gap-2'>
+                                <div className='w-8 h-8 bg-red-400 self-center border-gray-400 border rounded-sm '></div>
+                                <p className='text-gray-100 self-center'>Alta</p>
+                            </div>
+                            
                         </div>
                     </div>
                 </div>
