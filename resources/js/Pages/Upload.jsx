c12da150bd38eedce842c93fa2fc6ebf47fcb145
Leonardo Furmann <leohitnerf@gmail.com>
2024-11-09 21:03:35 -0300

    verificação imagem

--- resources/js/Pages/Upload.jsx
+++ resources/js/Pages/Upload.jsx
@@ -1,33 +1,58 @@
 import React, { useState, useCallback  } from 'react'
 import { useDropzone } from 'react-dropzone';
+import Alert from '@/Components/Utils/Alert';
 
 const Upload = ({setImage, setImageBinary}) => {
+    const [error, setError] = useState();
 
     const processFile = (file) => {
         const reader = new FileReader();
         
         reader.onloadend = () => {
-            const binaryStr = reader.result.split(',')[1];
-            setImageBinary(binaryStr)
-        };
+            const image = new Image();
+            image.onload = () => {
+               
+                if (image.width < 950 || image.height < 750) {
+                    setError("A imagem deve ter no mínimo 950x750 pixels.");
+                    return;
+                }
           
-        reader.readAsDataURL(file);  
-        setImage(URL.createObjectURL(file))
-    }
+                const binaryStr = reader.result.split(',')[1];
+                setImageBinary(binaryStr); 
+                setImage(URL.createObjectURL(file)); 
+            };
+
+            image.src = reader.result; 
+        };
+
+        reader.readAsDataURL(file); 
+    };
 
     const uploadImagem = (e) => {
         e.preventDefault();
         const file = e.target.files[0];
+        const fileType = file.type.toLowerCase();
+        if (!(fileType === 'image/png' || fileType === 'image/jpeg' || fileType === 'image/jpg')) {
+            setError("O arquivo deve ser uma imagem PNG, JPEG ou JPG.");
+            return;
+        }
+        setErro(null);
         processFile(file);
     }
 
     const onDrop = useCallback((acceptedFiles) => {
         const file = acceptedFiles[0];
+        const fileType = file.type.toLowerCase();
+        if (!(fileType === 'image/png' || fileType === 'image/jpeg' || fileType === 'image/jpg')) {
+            setError("O arquivo deve ser uma imagem PNG, JPEG ou JPG.");
+            return;
+        }
+        setError(null);
         processFile(file);
       }, []);
 
     const { getRootProps, getInputProps, isDragActive } = useDropzone({
-        onDrop 
+        onDrop ,
       });
 
     return (
@@ -38,10 +63,12 @@ const Upload = ({setImage, setImageBinary}) => {
                         <path stroke="#51B6FF" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M13 13h3a3 3 0 0 0 0-6h-.025A5.56 5.56 0 0 0 16 6.5 5.5 5.5 0 0 0 5.207 5.021C5.137 5.017 5.071 5 5 5a4 4 0 0 0 0 8h2.167M10 15V6m0 0L8 8m2-2 2 2" />
                     </svg>
                     <p className="mb-2 text-gray-500 dark:text-gray-400 font-semibold text-base">Clique ou arraste para fazer o upload da imagem de Raio-X</p>
-                    <p className="text-xs text-gray-500 dark:text-gray-400">PNG, JPG(MAX. 800x400px)</p>
+                    <p className="text-xs text-gray-500 dark:text-gray-400">PNG, JPG(MIN. 950x750px)</p>
                 </div>
                 <input {...getInputProps()} id="dropzone-file" type="file" className="hidden" onChange={uploadImagem} />
             </label>
+
+           {error && <Alert message={error} type={error} />} 
         </div>
     )
 }
