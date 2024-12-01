ae479c7bf7278a7ba0f5c512d34a3ceedbe0b8f8
Santian <joaovs@whbbrasil.com.br>
2024-12-01 14:28:37 -0300

    Algumas correcoes

--- resources/js/Components/Header.jsx
+++ resources/js/Components/Header.jsx
@@ -31,7 +31,7 @@ const Header = ({ user }) => {
                                         type="button"
                                         className="inline-flex items-center px-2 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500  hover:text-gray-700 focus:outline-none transition ease-in-out duration-150"
                                     >
-                                        <img src={perfil} alt="Icone de perfil" className='w-10'/>
+                                        <img src={perfil} alt="Icone de perfil" className='w-7'/>
 
                                     </button>
                                 </span>
--- resources/js/Pages/Upload.jsx
+++ resources/js/Pages/Upload.jsx
@@ -63,9 +63,9 @@ const Upload = ({setImage, setImageBinary}) => {
                         <path stroke="#51B6FF" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M13 13h3a3 3 0 0 0 0-6h-.025A5.56 5.56 0 0 0 16 6.5 5.5 5.5 0 0 0 5.207 5.021C5.137 5.017 5.071 5 5 5a4 4 0 0 0 0 8h2.167M10 15V6m0 0L8 8m2-2 2 2" />
                     </svg>
                     <p className="mb-2 text-gray-500 dark:text-gray-400 font-semibold text-base">Clique ou arraste para fazer o upload da imagem de Raio-X</p>
-                    <p className="text-xs text-gray-500 dark:text-gray-400">PNG, JPG(MIN. 950x750px)</p>
+                    <p className="text-xs text-gray-500 dark:text-gray-400">PNG, JPG (MIN. 950 x 750)</p>
                 </div>
-                <input {...getInputProps()} id="dropzone-file" type="file" className="hidden" onChange={uploadImagem} />
+                <input {...getInputProps()} type="file" className="hidden" onChange={uploadImagem} />
             </label>
 
            {error && <Alert message={error} type={error} />} 
